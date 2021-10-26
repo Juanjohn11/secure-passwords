f2ccd961d284cc5ce9c121e79d66dd4443952eb4
Juanjohn11 <elmatanaco1144@gmail.com>
2021-10-25 23:16:45 -0700

    added for loop fixed minor bugs which made the code shoot out undfined

--- assets/script.js
+++ assets/script.js
@@ -1,69 +1,90 @@
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-
+function ran(low, high) {
+     low = Math.ceil(low);
+     high = Math.floor(high);
+     
+     return Math.floor(Math.random() * (high - low) + low);
+}
 function passwordSize(){
-     var charaLength = window.prompt("Length of password must be between 8 to 128.");{
-         while (charaLength < 8 || charaLength > 128){
+     var charaLength = window.prompt("Length of password must be between 8 to 128.");
+      
+     while (charaLength < 8 || charaLength > 128){
         charaLength = window.prompt("Please enter a number betwenn 8 to 128.")
              
-        }
      }
      
-};
+     return(charaLength)
+}
 
-function generatePassword() {
+function ranChara(lower, upper, number, symbol) {
      //create vars for each chara type
+     var characters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J","K", "L", "M", "N", "O", "P","Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f","g", "h",
+     "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v","w", "x", "y","z", "1", "2", "3", "4", "5", "6", "7", "8",
+     "9", "0", "!", "#","$", "%", "&", "'", "(", ")",
+     "*", "+", ",","-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~", "//"];
 
-     var upperCase = ["A", "B", "C", "D", "E", "F", "G",
-
-     "H", "I", "J","K", "L", "M", "N", "O", "P","Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
-     ]; 
-     var lowerCase = ["a", "b", "c", "d", "e", "f","g", "h",
-     "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v","w", "x", "y","z",
-     ]; 
-
-     var numbers = ["1", "2", "3", "4", "5", "6", "7", "8",
-     "9", "0",
-     ];
-
-     var symbols = ["!", "#","$", "%", "&", "'", "(", ")",
-     "*", "+", ",","-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"
-     ];
-    // if statments not 8 - 128 alert invaild length and return
-    //prompt the user for the length of password
+     if(lower == false){
+          characters.splice(62 ,(characters.length-62));
+          } 
+     if (upper == false){
+          characters.splice(52, 10)
+          }
+     if (number == false){
+          characters.splice(26, 26)
+          }
+     if (symbol == false) {
+          characters.splice(0, 26)
+          }
+     return(characters);        
     
+}  
+function generatePassword(){
+     var pass = 0;
+     var securePassword = '';
+     var length = passwordSize();
+     while(pass == 0){
+          var lower = window.confirm("Would you like lowercase letters?");
+         
+          var upper = window.confirm("Would you like uppercase letter?");
    
-    //confirm the user for all characters
-    var lower = window.confirm("Would you like lowercase letters?");
+          var number = window.confirm("Would you like numbers?");
+
+          var symbol = window.confirm("Would you like symbols?");
+          
+          if (lower === false && upper === false && number === false && symbol === false){
+               window.alert("Must choose at least one."); 
+          
+          }
+          else{
+               pass++;
+          } 
+     }
+     var characters = ranChara(lower, upper, number, symbol);
      
-     
-    var upper = window.confirm("Would you like uppercase letter?");
+     for (i = 0; i < length ; i++) {
+          var securePassword = securePassword + characters[ran(0, characters.length)]; 
+     } 
 
+     return(securePassword);
 
-    var number = window.confirm("Would you like numbers?");
+}
 
+   
+//confirm the user for all characters
+//if all are false alert must have one and return 
+//else wind.alert must have one at least
+//use if statments and confirm vars to create charas string 
+//for loop to run below multipe times
 
-    var symbol = window.confirm("Would you like symbols?");
+// have math.random to get a random character from charas 
+//return password
+    
+// if statments not 8 - 128 alert invaild length and return
+//prompt the user for the length of password
+
          
      
-     if (lower === false && upper === false && number === false && symbol === false){
-          window.alert("Must choose at least one.") 
-          
-     } 
-    
-     
-
-        
-    
- 
-    //if all are false alert must have one and return 
-    //else wind.alert must have one at least
-    //use if statments and confirm vars to create charas string 
-    //for loop to run below multipe times
-    //for (var i = 0 < charaLength; i++){} 
-    // have math.random to get a random character from charas 
-    //return password
-};
+var generateBtn = document.querySelector("#generate");    
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
